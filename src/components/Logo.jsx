029c303b964c8b46cb73ccf9f53d69ada1738b6e
Ayush Patel <patelayush01jan@gmail.com>
2025-04-03 14:41:56 +0530

    added logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import img from "../images/logo.webp";
 
-function Logo({ width = "100px" }) {
-	return <div>Logo</div>;
+function Logo() {
+	return (
+		<div className="flex items-center justify-center rounded-lg bg-gray-500">
+			<img
+				src={img}
+				alt="Logo"
+				className="w-10 h-10 object-contain rounded-lg shadow-md hover:shadow-lg hover:scale-110 transition-transform duration-300"
+			/>
+		</div>
+	);
 }
 
 export default Logo;
